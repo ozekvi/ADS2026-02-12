366e675b137d05f1b7b1485eeefb2b79ee9fa8c0
oZekvi <oookiooo1707@gmail.com>
2026-03-12 00:00:19 +0300

    lesson3 Task1+Task2+Task3

--- src/by/it/group551002/marochkov/lesson02/A_VideoRegistrator.java
+++ src/by/it/group551002/marochkov/lesson02/A_VideoRegistrator.java
@@ -2,7 +2,6 @@ package by.it.group551002.marochkov.lesson02;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
 /*
 Даны события events
 реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
@@ -38,14 +37,7 @@ public class A_VideoRegistrator {
         //вычислим момент окончания работы видеокамеры
         //и теперь пропустим все покрываемые события
         //за время до конца работы, увеличивая индекс
-        Arrays.sort(events);
 
-        for (int j = 0; j < events.length; j++) {
-            if (events[i] + workDuration <= events[j]) {
-                result.add(events[i]);
-                i = j;
-            }
-        }
 
         return result;                        //вернем итог
     }
